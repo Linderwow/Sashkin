1b511c038f01ff1802de8da7ce5caf67cc57bfb7
Linderwow <alexyiyi92@gmail.com>
2022-01-20 21:41:04 -0500

    Step 2

--- src/GOLOVA.java
+++ src/GOLOVA.java
@@ -1,5 +1,6 @@
 public class GOLOVA {
     public static void main(String[] args) {
         System.out.println("We are great coders");
+        System.out.println("Katya is working at Netflix");
     }
 }
